98e53428369c69b4ad2e4e05d4c08f7b8808a0de
Roberto Lizana <robertolizana@trey.es>
2015-01-09 01:27:56 +0100

    [DEL] Eliminado log de debug olvidado

--- website_sale_product_gallery/models/view.py
+++ website_sale_product_gallery/models/view.py
@@ -18,11 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
-import logging
 from openerp.osv import osv
-
-logger = logging.getLogger(__name__)
 
 
 class View(osv.osv):
@@ -49,14 +45,9 @@ class View(osv.osv):
               Si no, devuelve las imágnes del template.
         """
         if obj._name == 'product.template':
-            logger.info((u"gallery --"))
-
             # comprobamos si teines atributos que afecten a la imagen
             att_affects_image = [a for a in obj.attribute_line_ids
                                  if a.attribute_id.affects_image]
-
-            logger.info(att_affects_image)
-
             if len(att_affects_image) > 0:
                 return self._get_gallery_product_product(
                     obj.product_variant_ids[0])
@@ -69,39 +60,21 @@ class View(osv.osv):
 
     def _get_gallery_product_template(self, template):
         """ Buscamos dentro de la galleria las imagenes sin atributos """
-        logger.info((u"*" * 30) + "  PRODUCT TEMPLATE " + str(template.id))
         res = [img for img in template.gallery_ids
                if len(img.attribute_value_ids) == 0]
-        logger.info(res)
-        logger.info((u"*" * 30))
         return res
 
     def _get_gallery_product_product(self, product):
         """ Buscamos las imagenes dentro de la gallery que concuerdan con la
         variante pasada """
-        logger.info((u"*" * 30) + "  PRODUCT PRODUCT " + str(product.id))
-
         attributes_values = [l.id for l in product.attribute_value_ids
                              if l.affects_image]
         attributes_values_len = len(attributes_values)
-
         gallery = []
         for img in product.product_tmpl_id.gallery_ids:
             rest = set(attributes_values) - \
                 set([l.id for l in img.attribute_value_ids])
-
-            # logger.info("{}: {} - {} = {}".format(
-            #     img.id,
-            #     set(attributes_values),
-            #     set([l.id for l in img.attribute_value_ids]),
-            #     rest))
-
             if len(rest) != attributes_values_len:
                 # alguno de los atributos casa con la imagen, lo mostramos
                 gallery.append(img)
-
-        logger.info("------")
-        logger.info(gallery)
-        logger.info((u"*" * 30))
-
         return gallery
