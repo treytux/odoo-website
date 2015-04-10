d2ba0dbd0e7dc77c17e3ff6d554402a4165bef4d
Joaquin Gutierrez <joaquin@trey.es>
2015-04-10 12:50:03 +0200

    [FIX]: website_ratings nueva api 8

--- website_ratings/models/ratings.py
+++ website_ratings/models/ratings.py
@@ -2,43 +2,65 @@
 # License, author and contributors information in:
 # __openerp__.py file at the root folder of this module.
 
-from openerp.osv import fields, orm
-from datetime import datetime
-
+from openerp import models, api, fields
 import logging
+
+# from openerp.osv import fields, orm
+# from datetime import datetime
+
 _log = logging.getLogger(__name__)
 
 
-class Ratings(orm.Model):
+class Ratings(models.Model):
     _name = "website_ratings.ratings"
-    _columns = {
-        'website_id': fields.many2one('website', string="Website"),
 
-        'object_id': fields.integer(u'Id del modelo', select=True),
-        'object_model': fields.char(u'Nombre del modelo', size=255,
-                                    translate=False),
-        'numbers_of_ratings': fields.integer(u'Número de valoraciones hechas '
-                                             u'sobre el modelo'),
-        'ratings': fields.float(u'Valoración media del objecto'),
-    }
-    _defaults = {
-        'numbers_of_ratings': 1,
-        'ratings': 0,
-    }
+    website_id = fields.Many2one(
+        comodel_name='website',
+        string="Website"
+    )
+    object_id = fields.Integer(
+        string=u'Id del modelo',
+        select=True
+    )
+    object_model = fields.Char(
+        string=u'Nombre del modelo',
+        size=255,
+        translate=False
+    )
+    numbers_of_ratings = fields.Integer(
+        string=u'Número de valoraciones hechas sobre el modelo',
+        default=1
+    )
+    ratings = fields.Float(
+        string=u'Valoración media del objecto',
+        default=0
+    )
 
 
-class UserRatings(orm.Model):
+class UserRatings(models.Model):
     _name = "website_ratings.user_rating"
-    _columns = {
-        'website_id': fields.many2one('website', string="Website"),
-        'user_id': fields.many2one('res.users', string="User id"),
 
-        'object_id': fields.integer(u'Id del modelo', select=True),
-        'object_model': fields.char(u'Nombre del modelo', size=255,
-                                    translate=False),
-        'date_rating': fields.datetime(u'Fecha de la valoración'),
-        'rating': fields.float(u'Valoración sobre el objeto del usuario'),
-    }
-    _defaults = {
-        'date_rating': lambda *a: datetime.now(),
-    }
+    website_id = fields.Many2one(
+        comodel_name='website',
+        string="Website"
+    )
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string="User id"
+    )
+    object_id = fields.Integer(
+        string=u'Id del modelo',
+        select=True
+    )
+    object_model = fields.Char(
+        string=u'Nombre del modelo',
+        size=255,
+        translate=False
+    )
+    date_rating = fields.Datetime(
+        string=u'Fecha de la valoración',
+        default=fields.Datetime.now()
+    )
+    rating = fields.Float(
+        string=u'Valoración sobre el objeto del usuario'
+    )
